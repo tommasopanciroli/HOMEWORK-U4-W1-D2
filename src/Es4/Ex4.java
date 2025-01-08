11333e7bf6318819c094f0d178706679a74fc2d9
tommaso.panciroli <tommypanci@gmail.com>
2025-01-08 15:08:15 +0100

    adds Ex4

---
+++ src/Es4/Ex4.java
@@ -0,0 +1,24 @@
+package Es4;
+
+import java.util.Scanner;
+
+public class Ex4 {
+
+    public static void Esercizio4 (String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Scrivi un numero e premi invio");
+        int test = scan.nextInt();
+        countdown(test);
+        scan.close();
+    }
+
+    public static void countdown(int num) {
+        System.out.println("Conto alla rovescia" + num + " a 0");
+
+        for (int i = num; i >= 0; i--) {
+            System.out.println(i);
+        }
+    }
+
+}
+
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 import Es1.Ex1;
 import Es2.Ex2;
-
+import Es4.Ex4;
 
 public class Main {
     public static void main(String[] args) {
@@ -11,6 +11,8 @@ public class Main {
 
         System.out.println(Ex2.stampaNumero(2));
         System.out.println(Ex2.stampaNumero(4));
+
+        
     }
 
 }
